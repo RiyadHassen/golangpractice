436b74816013327fc90bf0a3df594b44020baa66
RiyadHassen <riyadworku@gmail.com>
2019-12-20 11:06:47 +0300

    map program main function

--- Exersice/MapFunction/mapfunction.go
+++ Exersice/MapFunction/mapfunction.go
@@ -1,19 +1,19 @@
 package main
 
-//higher order function
-//func square(x int) int  {
-//	return x * x
-//
-//}
+import "fmt"
 
 func mapfunc(square func(x int) int, lst []int) []int {
 	maps := []int{}
 	for i := 0; i < len(lst); i++ {
-		maps = append(maps, square(i))
+		maps = append(maps, square(lst[i]))
 	}
 	return maps
 }
 
 func main() {
-
+	lst := []int{2, 3, 4, 5}
+	f := func(x int) int {
+		return x * x
+	}
+	fmt.Println(mapfunc(f, lst))
 }
