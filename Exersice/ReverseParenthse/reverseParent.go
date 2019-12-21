c083212bd1478d81de69f763360d0aa0cade6ecc
RiyadHassen <riyadworku@gmail.com>
2019-12-21 05:34:19 +0300

    check parentse and reverse method used reverseParen method added and tested in main fucntion

--- Exersice/ReverseParenthse/reverseParent.go
+++ Exersice/ReverseParenthse/reverseParent.go
@@ -2,27 +2,62 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func reverseParen(s string) string {
+	revpar := ""
 	rev := ""
 	if checkpar(s) {
 		for i := 0; i < len(s); i++ {
 			if s[i] == '(' {
 				for j := i + 1; j < len(s); j++ {
+
 					if s[j] == ')' {
+						i = j
+						fmt.Println(i, j)
 						break
 					} else {
-						char := string(s[j])
-						rev = rev + char
+						if s[j] == '(' {
+
+						} else {
+							char := string(s[j])
+							rev = rev + char
+						}
+
 					}
+				}
+
+				rev = reverse(rev)
+				revpar += rev
+
+			} else if s[i] == ')' {
+
+			} else {
+				if s[i] == '(' {
+					revpar += ""
+				} else {
+					revpar += string(s[i])
 				}
 			}
 		}
 	}
-
+	return revpar
 }
+func reverse(s string) string {
+	rev := ""
+	for i := len(s) - 1; i >= 0; i-- {
+		rev += string(s[i])
+	}
+	return rev
+}
+
+func main() {
+	fmt.Println(checkpar("(bar)"))
+	fmt.Println(checkpar("foo(bar(baz))blim"))
+	fmt.Println(reverseParen("foo(bar(baz))blim"))
+	fmt.Println(reverseParen("foo(bar)bar"))
+}
+
 func checkpar(s string) bool {
 	close, open := false, false
 	for i := 0; i < len(s); i++ {
@@ -41,17 +76,4 @@ func checkpar(s string) bool {
 	} else {
 		return false
 	}
-
-}
-func reverse(s string) string {
-	rev := ""
-	for i := len(s) - 1; i >= 0; i-- {
-		rev += string(s[i])
-	}
-	return rev
-}
-
-func main() {
-	fmt.Println(checkpar("(bar)"))
-	fmt.Println(checkpar("foo(bar(baz))blim"))
 }
