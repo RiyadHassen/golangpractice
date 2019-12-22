c5096354370483fae21a47cfaa8ccc5a79244d58
RiyadHassen <riyadworku@gmail.com>
2019-12-22 04:33:15 +0300

    enceding decoding decoding decoding and return value functions added

--- Exersice/RomanNo/conversion.go
+++ Exersice/RomanNo/conversion.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func encode(n int) (bool, string) {
 	decimal := []int{1, 4, 5, 9, 10, 40,
@@ -29,8 +31,53 @@ func encode(n int) (bool, string) {
 	}
 
 }
+func decode(str string) (bool, int) {
 
+	roman := []string{
+		"I", "IV", "V", "IX",
+		"X", "XL", "L", "XC",
+		"C", "CD", "D", "CM", "M"}
+	num := 0
+
+	for i := 0; i < len(str)-1; i++ {
+		for j := 0; j < len(roman); j++ {
+			if relateValue(string(str[i])) > relateValue(string(str[i+1])) {
+				num += relateValue(string(str[i]))
+			} else {
+				num -= relateValue(string(str[i]))
+				num += relateValue(string(str[i+1]))
+			}
+		}
+	}
+	if num > 0 {
+		return true, num
+	} else {
+		return false, num
+	}
+
+}
+
+func relateValue(s string) int {
+	decimal := []int{1, 4, 5, 9, 10, 40,
+		50, 90, 100, 400,
+		500, 900, 1000}
+	roman := []string{
+		"I", "IV", "V", "IX",
+		"X", "XL", "L", "XC",
+		"C", "CD", "D", "CM", "M"}
+	val := 0
+	for i := 0; i < len(roman); i++ {
+		if roman[i] == s {
+			val = decimal[i]
+			break
+		}
+	}
+	return val
+
+}
 func main() {
 	r, e := encode(1990)
 	fmt.Println(r, e)
+	d, no := decode("MMVIII")
+	fmt.Println(d, no)
 }
